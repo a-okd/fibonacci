b6b98c285848cabcdba57e93fd8149076bc029ab
Aozora Okada <bluesky125osmkk@gmail.com>
2020-09-12 15:57:26 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict';
 const memo = new Map();
+// n=0とn=1は決まっているものなので最初から入れておく
 memo.set(0, 0);
 memo.set(1, 1);
+
 function fib0(n) {
     if (n === 0) {
         return 0;
@@ -12,14 +14,17 @@ function fib0(n) {
 }
 
 function fib(n) {
+    // メモにデータがあればそれを使う
     if (memo.has(n)) {
         return memo.get(n);
+    } else {
+        // メモにデータがないので計算する
+        const value = fib(n - 1) + fib(n - 2);
+        memo.set(n, value);
+        return value;
     }
-    const value = fib(n - 1) + fib(n - 2);
-    memo.set(n, value);
-    return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(i + "のフィボナッチ数は " + fib0(i) + " です。");
+    console.log(i + "のフィボナッチ数は " + fib(i) + " です。");
 }
